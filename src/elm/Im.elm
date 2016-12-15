60bd2afecbb0c18d58809b68f77f52701f4ab4fc
Fredrik Gustafsson <fredrik@infomaker.se>
2016-12-15 15:59:21 +0100

    Elm cleanup

--- src/elm/Im.elm
+++ src/elm/Im.elm
@@ -15,7 +15,6 @@ room very similar to the one here.
 -}
 
 import Html exposing (..)
-import WebSocket as WS
 
 
 main : Program String Model Msg
@@ -33,15 +32,14 @@ main =
 
 
 type alias Model =
-    { server : String
-    , input : String
+    { input : String
     , messages : List String
     }
 
 
 init : String -> ( Model, Cmd Msg )
-init server =
-    ( Model server "" [], Cmd.none )
+init model =
+    ( Model "" [], Cmd.none )
 
 
 
@@ -49,36 +47,13 @@ init server =
 
 
 type Msg
-    = Input String
-    | Send
+    = Send
     | UpdateCounter Int
-    | NewMessage String
 
 
-{-| Our update function reacts to a few different messages.
-
-    1. Typing into the text field
-    2. Clicking the "Send" button
-    3. Emoji sent in from JavaScript
-    4. Messages received from the chat server
-
-The return value of this function is a pair. First is the
-updated model. In many cases, we are just modifying the input
-field. Second is some commands we want to run. We only use
-this with `Send` when we want to send a message to the chat
-server.
-
-Again, check out <http://guide.elm-lang.org/architecture> to
-learn more about how this works!
--}
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg ({ server, input, messages } as model) =
+update msg ({ input, messages } as model) =
     case msg of
-        Input newInput ->
-            ( { model | input = newInput }
-            , Cmd.none
-            )
-
         Send ->
             ( model, output (model.messages) )
 
@@ -92,37 +67,21 @@ update msg ({ server, input, messages } as model) =
             in
                 update Send m
 
-        NewMessage newMessage ->
-            ( { model | messages = newMessage :: messages }
-            , Cmd.none
-            )
-
 
 
 -- SUBSCRIPTIONS
 
 
-{-| This port lets outsiders send in emoji characters as a string.
-We can subscribe to these messages from within Elm.
--}
 port counter : (Int -> msg) -> Sub msg
 
 
 port output : List String -> Cmd msg
 
 
-{-| We subscribe to two kinds of messages.
-
-  1. We want messages from the websocket chat server.
-  2. We want messages sent through emoji port from JavaScript.
-
--}
 subscriptions : Model -> Sub Msg
-subscriptions { server } =
+subscriptions { input } =
     Sub.batch
-        [ WS.listen server NewMessage
-        , counter UpdateCounter
-        ]
+        [ counter UpdateCounter ]
 
 
 
@@ -132,20 +91,3 @@ subscriptions { server } =
 view : Model -> Html Msg
 view _ =
     div [] []
-
-
-
-{- }
-   view : Model -> Html Msg
-   view model =
-       div [ class "chat-container" ]
-           [ input [ class "chat-message-input", onInput Input, value model.input ] []
-           , button [ onClick Send ] [ text "Send" ]
-           , div [ class "chat-messages" ] (List.map viewMessage (List.reverse model.messages))
-           ]
--}
-
-
-viewMessage : String -> Html msg
-viewMessage msg =
-    div [] [ text msg ]
